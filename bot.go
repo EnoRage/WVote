a595b3b1c1f6d97b25c54071aa74d2b9683e0600
Your Name <you@example.com>
2018-04-06 23:32:29 +0300

    main menu init

--- bot.go
+++ bot.go
@@ -19,9 +19,32 @@ func main() {
 		return
 	}
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "hello world")
-	})
+	// Кнопки и меню
+	mainData := tb.ReplyButton{Text: "💳 Мой кабинет"}
+	votingData := tb.ReplyButton{Text: "Текущие голосования"}
+	mainMenu := [][]tb.ReplyButton{{mainData}, {votingData}}
 
+	viewNew := tb.InlineButton{Unique: "viewnew", Text: "Текущие голосования"}
+	viewRes := tb.InlineButton{Unique: "viewres", Text: "История моих голосований"}
+	viewMenu := [][]tb.InlineButton{{viewNew}, {viewRes}}
+	// Кнопки и меню
+
+	// Обработчики на главное меню
+	b.Handle("/start", func(m *tb.Message) {
+		b.Send(m.Sender, "Главное меню", &tb.ReplyMarkup{ResizeReplyKeyboard: true, ReplyKeyboard: mainMenu})
+	})
+	b.Handle(&mainData, func(m *tb.Message) {
+		var msg = "Ваш публичный адрес: "
+		// msg += addressPub
+		msg += "\n\nВаш seed: "
+		// msg += seed
+		msg += "\n\nВаш баланс: "
+		// msg += balance
+		msg += " (РУБ)"
+		msg += "\n\nТут вы можете найти открытые голосования и посмотреть результаты прошлых"
+
+		b.Send(m.Sender, msg, &tb.ReplyMarkup{InlineKeyboard: viewMenu})
+	})
 	b.Start()
+	// Обработчики на главное меню
 }
