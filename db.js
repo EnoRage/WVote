8b03a5ba2699ad15d3990b0abf022256f39b8dd0
Кирилл <krboktv@gmail.com>
2018-04-07 02:10:11 +0300

    Поиск доступных голосований

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose"),
-    ObjectId = require('mongoose').Types.ObjectId; 
-    Users = require('./schemas/usersSchema.js');
-    Organizations = require('./schemas/organisationsSchema.js'),
+    ObjectId = require('mongoose').Types.ObjectId;
+Users = require('./schemas/usersSchema.js');
+Organizations = require('./schemas/organisationsSchema.js'),
     Votes = require('./schemas/votes.js').votes;
-    Voters = require('./schemas/votes.js').voters;
+Voters = require('./schemas/votes.js').voters;
 
 mongoose.Promise = global.Promise;
 
@@ -22,57 +22,96 @@ const db = mongoose.connect(uri).then(console.log('Mongo DB works fine'));
 
 // Добавление организации в базу данных
 function addOrganisation(name, description, approvedAddresses) {
-    Organizations.create({name: name, description: description, approvedAddresses: approvedAddresses}, (err,doc) => {
-        
-    })
+    Organizations.create({
+        name: name,
+        description: description,
+        approvedAddresses: approvedAddresses
+    }, (err, doc) => {
+
+    });
 }
 
-// Поиск доступных адресов для голсования
+// Поиск доступных адресов для голсования конкретной организации
 function findApprovedAddresses(organisationID, callback) {
-    Organizations.find({_id: new ObjectId(organisationID)}, (err,doc) => {
+    Organizations.find({
+        _id: new ObjectId(organisationID)
+    }, (err, doc) => {
         callback(doc[0].approvedAddresses);
-    })
+    });
+}
+
+// Поиск доступных голосований
+function findApprovedAddressesInAll(address, callback) {
+    var organisationsID = [];
+
+    Organizations.find({}, (err, organisations) => {
+        for (let i in organisations) {
+            for (let j in organisations[i].approvedAddresses) {
+                if (organisations[i].approvedAddresses[j] == address) {
+                    organisationsID.push(organisations[i]._id);
+                }
+            }
+        }
+
+        callback(organisationsID);
+    });
 }
 
 // Создание нового пользователя
 function addUser(userID, name, encryptedSeed, address) {
-    Users.create({userID: userID, name: name, encryptedSeed: encryptedSeed, address: address}, (err,doc) => {
-        
-    })
+    Users.create({
+        userID: userID,
+        name: name,
+        encryptedSeed: encryptedSeed,
+        address: address
+    }, (err, doc) => {
+
+    });
 }
 
 // Поиск пользователя по userID
 function findUserByUserID(userID, callback) {
-    Users.find({userID: userID}, (err,doc) => {
+    Users.find({
+        userID: userID
+    }, (err, doc) => {
         callback(doc[0]);
-    })
+    });
 }
 
 // Поиск пользователя по адресу
 function findUserByAddress(address, callback) {
-    Users.find({address: address}, (err,doc) => {
+    Users.find({
+        address: address
+    }, (err, doc) => {
         callback(doc[0]);
     })
 }
 
 // Поиск всех пользователей
 function findAllUsers(address, callback) {
-    Users.find({}, (err,doc) => {
+    Users.find({}, (err, doc) => {
         callback(doc);
-    })
+    });
 }
 
 
 // Создание голосования
 function createVote(organizationID, description) {
-    Votes.create({organizationID: new ObjectId(organizationID), description: description}, (err,doc) => {
+    Votes.create({
+        organizationID: new ObjectId(organizationID),
+        description: description
+    }, (err, doc) => {
 
-    })
+    });
 }
 
 // Принять участие в голосовании
 function takePartInVote(voteID, description) {
-    Votes.create({voteID: new ObjectId(voteID), address: address, vote: vote}, (err,doc) => {
+    Votes.create({
+        voteID: new ObjectId(voteID),
+        address: address,
+        vote: vote
+    }, (err, doc) => {
 
-    })
+    });
 }
